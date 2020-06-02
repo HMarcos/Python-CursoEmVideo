0dde7f1bf7d5fc2174c96216e618c795a329f14d
Marcos Henrique <marcosmarcone48@gmail.com>
2020-06-02 20:01:25 -0300

    Aula 06: Tipos Primitivos

---
+++ Scripts/002.1-tipos_primitivos.py
@@ -0,0 +1,17 @@
+print("PARTE 1")
+nome = input('Informe o seu nome: ')
+n1 = int(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+a = True
+s = n1 + n2
+
+print('A soma entre os {0} e {1} vale: {2}'.format(n1, n2, s))
+print('----------------------------------')
+print('Tipo primitivo de n1: ', type(n1))
+print('Tipo primitivo de n2: ', type(n2))
+print('Tipo primitivo de s: ', type(s))
+print('Tipo primitivo de nome: ', type(nome))
+print('Tipo primitivo de a: ', type(a))
+
+
+
---
+++ Scripts/002.2-metodos_das_strigs.py
@@ -0,0 +1,7 @@
+print('PARTE 2')
+n = input('Digite algo: ')
+print(n.isnumeric())  # Se pode ser convertido em um número
+print(n.isalpha())  # Se são apenas letras
+print(n.isalnum())  # Se são letras ou números
+print(n.isupper())  # Se todas os caracteres são maiúsculos
+print(n.islower())  # Se todas os caracteres são minúsculos
